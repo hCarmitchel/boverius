c87614f7045bcd972c1300028c8f74ca80547560
Heather Carmitchel <heather.carmitchel@cerner.com>
2017-02-03 17:04:55 -0600

    Test logs

--- config/environment.rb
+++ config/environment.rb
@@ -1,11 +1,5 @@
 # Load the Rails application.
 require File.expand_path('../application', __FILE__)
 
-if !Rails.env.development
-  Rails.logger = Le.new('LOGENTRIES_TOKEN')
-else
-  Rails.logger = Le.new('LOGENTRIES_TOKEN', local: true)
-end
-
 # Initialize the Rails application.
 Rails.application.initialize!
